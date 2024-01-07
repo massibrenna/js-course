4776dd81c5471fd48020836d4802e7029cd6b0ab
USERSAD\EBRENNMEI <massimiliano.brenna@emeal.nttdata.com>
2024-01-07 17:48:23 +0100

    first ccommit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+window.onload = () => {
+
+    // Create a new paragraph element, and append it to the end of the document body
+    const p = document.createElement("p");
+    p.innerHTML ='dfdf dfdff';
+    document.body.appendChild(p);
+
+
+    
+    
+}
+//document.body.innerHTML = 'some sub content';
